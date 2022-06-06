29794d35cf7680e21f3fd5704b3cad5fd5322eb4
Joel <jcz@hey.com>
2022-06-06 12:23:46 -0700

    fix: properly capitalize head tags

--- app.vue
+++ app.vue
@@ -18,15 +18,15 @@ onBeforeMount(() => {
     <div>
         <Head>
             <Title>Sana's Space Birthday Project</Title>
-            <meta name="viewport" content="width=device-width, initial-scale=0.86, minimum-scale=0.86">
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1">
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1">
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1">
-            <link rel="manifest" href="/site.webmanifest?v=1">
-            <link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5">
-            <link rel="shortcut icon" href="/favicon.ico?v=1">
-            <meta name="msapplication-TileColor" content="#2b5797">
-            <meta name="theme-color" content="#1b2735">
+            <Meta name="viewport" content="width=device-width, initial-scale=0.86, minimum-scale=0.86"/>
+            <Link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=1"/>
+            <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=1"/>
+            <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=1"/>
+            <Link rel="manifest" href="/site.webmanifest?v=1"/>
+            <Link rel="mask-icon" href="/safari-pinned-tab.svg?v=1" color="#5bbad5"/>
+            <Link rel="shortcut icon" href="/favicon.ico?v=1"/>
+            <Meta name="msapplication-TileColor" content="#2b5797"/>
+            <Meta name="theme-color" content="#1b2735"/>
         </Head>
         <NuxtPage/>
     </div>
