f5088219eba37430e492623ea6540165c1c9fd57
Joel <jcz@hey.com>
2022-06-05 23:44:21 -0700

    fix: try using vw and vh on .card-container for better scaling

--- pages/index.vue
+++ pages/index.vue
@@ -226,8 +226,8 @@ const onArtModalOpen = () => {
     position: absolute;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
     z-index: 2;
     display: flex;
     flex-direction: column;
